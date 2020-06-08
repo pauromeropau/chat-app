cac51447c06be0be14e5ffa3748780b0c61fee3e
Paula <pauromeropau@gmail.com>
2020-06-08 13:41:07 +0100

    done

---
+++ client/src/components/TextContainer/TextContainer.js
@@ -0,0 +1,37 @@
+  
+import React from 'react';
+
+import onlineIcon from '../../icons/onlineIcon.png';
+
+import './TextContainer.css';
+
+const TextContainer = ({ users }) => (
+  <div className="textContainer">
+    <div>
+      <h1>Realtime Chat App <span role="img" aria-label="emoji">💬</span></h1>
+      <h2>With React, Express, Node and Socket.io</h2>
+
+    </div>
+    {
+      users
+        ? (
+          <div>
+            <h1>Currently chatting:</h1>
+            <div className="activeContainer">
+              <h2>
+                {users.map(({name}) => (
+                  <div key={name} className="activeItem">
+                    {name}
+                    <img alt="Online Icon" src={onlineIcon}/>
+                  </div>
+                ))}
+              </h2>
+            </div>
+          </div>
+        )
+        : null
+    }
+  </div>
+);
+
+export default TextContainer;
--- server/index.js
+++ server/index.js
@@ -1,30 +1,51 @@
-const express = require("express");
-const socketio = require("socket.io");
-const http = require("http");
+const http = require('http');
+const express = require('express');
+const socketio = require('socket.io');
+const cors = require('cors');
 
-const PORT = process.env.PORT || 5000;
+const { addUser, removeUser, getUser, getUsersInRoom } = require('./users');
 
-const router = require("./router");
+const router = require('./router');
 
 const app = express();
 const server = http.createServer(app);
 const io = socketio(server);
 
-io.on("connection", (socket) => {
-  console.log("We have a new connection!!!");
+app.use(cors());
+app.use(router);
 
-  socket.on("join", ({ name, room }, callback) => {
-    console.log(name, room);
+io.on('connect', (socket) => {
+  socket.on('join', ({ name, room }, callback) => {
+    const { error, user } = addUser({ id: socket.id, name, room });
+
+    if(error) return callback(error);
+
+    socket.join(user.room);
+
+    socket.emit('message', { user: 'Chatbot', text: `Hi, ${user.name}. Welcome to ${user.room}!`});
+    socket.broadcast.to(user.room).emit('message', { user: 'Chatbot', text: `${user.name} has joined!` });
+
+    io.to(user.room).emit('roomData', { room: user.room, users: getUsersInRoom(user.room) });
+
     callback();
-
-  socket.on("disconnect", () => {
-    console.log("User had left!!");
   });
 
+  socket.on('sendMessage', (message, callback) => {
+    const user = getUser(socket.id);
+
+    io.to(user.room).emit('message', { user: user.name, text: message });
+
+    callback();
+  });
+
+  socket.on('disconnect', () => {
+    const user = removeUser(socket.id);
+
+    if(user) {
+      io.to(user.room).emit('message', { user: 'ChatBot', text: `${user.name} has left.` });
+       io.to(user.room).emit('roomData', { room: user.room, users: getUsersInRoom(user.room)});
+    }
+  })
 });
 
-app.use(router);
-})
-server.listen(PORT, () => console.log(`Server has started on port ${PORT}`))
-
-
+server.listen(process.env.PORT || 5000, () => console.log(`Server has started.`));
